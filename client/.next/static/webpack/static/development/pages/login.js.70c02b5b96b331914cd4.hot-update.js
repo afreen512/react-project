b96d6bc2e609d33b4665dd7c92f0e9f59fa07aa1
afreen512 <afreenhussain866@gmail.com>
2020-07-16 11:27:33 +0500

    token

---
+++ client/.next/static/webpack/static/development/pages/login.js.70c02b5b96b331914cd4.hot-update.js
@@ -0,0 +1,19 @@
+webpackHotUpdate("static\\development\\pages\\login.js",[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */
+false,
+/* 4 */
+/*!******************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Flogin&absolutePagePath=C%3A%5CUsers%5CAfreen%20Hussain'%5CDesktop%5Caws-pro%5Cclient%5Cpages%5Clogin.js ***!
+  \******************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Flogin&absolutePagePath=C%3A%5CUsers%5CAfreen%20Hussain'%5CDesktop%5Caws-pro%5Cclient%5Cpages%5Clogin.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Flogin&absolutePagePath=C%3A%5CUsers%5CAfreen%20Hussain'%5CDesktop%5Caws-pro%5Cclient%5Cpages%5Clogin.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdGF0aWNcXGRldmVsb3BtZW50XFxwYWdlc1xcbG9naW4uanMuNzBjMDJiNWI5NmIzMzE5MTRjZDQuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
