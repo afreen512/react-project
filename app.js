9f87829d607d55d60201614ba8d5fb1350c1c17e
afreen512 <afreenhussain866@gmail.com>
2020-07-19 12:33:53 +0500

    commit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/api', linkRoutes);
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static('client/.next/static'));
   }
 
 const port = process.env.PORT || 8000;
